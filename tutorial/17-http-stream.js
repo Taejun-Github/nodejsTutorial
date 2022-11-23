e41f6a43956669f03c52011ec846c0a5452cac48
Taejun Kim <tjkimin2021@gmail.com>
2022-11-23 16:10:03 +0900

    http

---
+++ tutorial/17-http-stream.js
@@ -0,0 +1,13 @@
+let http = require('http');
+let fs = require('fs');
+
+http.createServer(function(req, res) {
+  const fileStream = fs.createReadStream('./content/big.txt', 'utf-8');
+  fileStream.on('open', () => {
+    fileStream.pipe(res)
+  })
+  fileStream.on('error', (err) => {
+    res.end(err);
+  })
+})
+.listen(5000)
